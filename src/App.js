73eb90ab7ba541f64450d78b0b9b06dca4b634d0
David Gaytan <edgaytanc@gmail.com>
2024-10-25 19:12:24 -0600

    Se agrego pagina publica

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
 import Home from './views/Home';
 import Clientes from './views/Clientes';
 import Productos from './views/Productos';
@@ -6,31 +7,34 @@ import Categorias from './views/Categorias';
 import Proveedores from './views/Proveedores';
 import Pedidos from './views/Pedidos';
 import Facturas from './views/Facturas';
-import React from 'react';
 import Dashboard from './components/Dashboard';
 import ListadoFacturas from './components/FacturasList';
 import Login from './components/Login';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Ruta para la página pública */}
+        <Route path="/" element={<HomePage />} />
+
         {/* Ruta para Login */}
         <Route path="/login" element={<Login />} />
-        
+
         {/* Rutas protegidas */}
         <Route
-          path="/*"
+          path="/dashboard/*"
           element={
             localStorage.getItem('token') ? (
               <Dashboard>
                 <Routes>
                   <Route path="/" element={<Home />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/dashboard" element={<Home />} />
                   <Route path="/clientes" element={<Clientes />} />
                   <Route path="/productos" element={<Productos />} />
-                  <Route path="/categorias" element={<Categorias/>} />
-                  <Route path="/proveedores" element={<Proveedores/>} />
+                  <Route path="/categorias" element={<Categorias />} />
+                  <Route path="/proveedores" element={<Proveedores />} />
                   <Route path="/pedidos" element={<Pedidos />} />
                   <Route path="/facturas" element={<Facturas />} />
                   <Route path="/facturaslista" element={<ListadoFacturas />} />
@@ -47,4 +51,3 @@ function App() {
 }
 
 export default App;
-
